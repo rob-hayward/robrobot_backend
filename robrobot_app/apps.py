4f3e76414cba96b87d15eb8c3cabfd6e86477ad2
rob-hayward <hayward.m.rob@gmail.com>
2024-04-30 10:52:40 +0100

    Initial commit

---
+++ robrobot_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RobrobotAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'robrobot_app'
---
+++ robrobot_app/serializers.py
@@ -0,0 +1,31 @@
+# robrobot_backend/serializers.py
+from rest_framework import serializers
+from .models import Profile, Project, GitRepository, ProjectImage
+
+
+class GitRepositorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GitRepository
+        fields = ['url', 'name']
+
+
+class ProjectImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectImage
+        fields = ['id', 'image']
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    git_repo_links = GitRepositorySerializer(many=True, read_only=True)
+    images = ProjectImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
