e7cf88e49ad094d9db4f35e232b6444a7033973d
rob-hayward <hayward.m.rob@gmail.com>
2024-08-28 19:06:22 +0100

    addeed root url

--- robrobot_backend/urls.py
+++ robrobot_backend/urls.py
@@ -1,12 +1,12 @@
-# robrobot_backend/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from robrobot_app.views import health_check, health_check_debug
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/', permanent=True)),  # Redirect root to /api/
     path('admin/', admin.site.urls),
     path('api/', include('robrobot_app.urls')),
     path('health/', health_check, name='health_check'),
