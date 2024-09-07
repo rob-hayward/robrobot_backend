cdd09553756bc8c82b198bfe857f73633342a7f1
rob-hayward <hayward.m.rob@gmail.com>
2024-09-07 19:08:16 +0100

    added 2 new projects

--- robrobot_backend/settings.py
+++ robrobot_backend/settings.py
@@ -21,10 +21,13 @@ ALLOWED_HOSTS = ['robhayward.io', 'www.robhayward.io', 'localhost', '127.0.0.1',
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '')  # This should be the root directory where 'project_images' and 'technology_icons' are located
 
 INSTALLED_APPS = [
     'corsheaders',
--- robrobot_backend/urls.py
+++ robrobot_backend/urls.py
@@ -1,4 +1,3 @@
-# /Users/rob/PycharmProjects/robrobot_backend/robrobot_backend/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -7,16 +6,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/api/', permanent=True)),  # Redirect root to /api/
+    path('', RedirectView.as_view(url='/api/', permanent=True)),
     path('admin/', admin.site.urls),
     path('api/', include('robrobot_app.urls')),
     path('health/', health_check, name='health_check'),
     path('health_debug/', health_check_debug, name='health_check_debug'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve static and media files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
